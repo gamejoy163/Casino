749d4fe696e2fdd12bfa91f89b4a5722b2e56451
Prosics <34700213+Prosics@users.noreply.github.com>
2018-01-06 17:09:17 +0800

    临时同步消息

--- Assets/_App/Scripts/UI/Login/LoginWndController.cs
+++ Assets/_App/Scripts/UI/Login/LoginWndController.cs
@@ -4,10 +4,14 @@
 // Copyright (c) 2018 ${Author}
 // Description:
 //
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Prosics;
 using Prosics.Utils;
 using Prosics.MVC;
+using Kimmidoll;
 
 
 namespace GameJoy
@@ -24,14 +28,36 @@ namespace GameJoy
 		protected override void OnInitialize ()
 		{
 			base.OnInitialize ();
+			NetMsgCenter.AddEventListener ((int)CProtocol.RLogin,OnLogin);
 			_wnd.eventClickLogin += OnClickLoginBtn;
-		}
 
+		}
+		protected override void Destroy ()
+		{
+			base.Destroy ();
+			NetMsgCenter.RemoveEventListener ((int)CProtocol.RLogin,OnLogin);
+		}
 		void OnClickLoginBtn(string account, string passwd)
 		{
 			Debug.Log ("click login:" + account + passwd);
 			NetMsgCenter.Request_Login (account,passwd);
 		}
+		bool isLogin = false;
+		void OnLogin(List<System.Object> args)
+		{
+			isLogin = true;
+
+		}
+		protected override void Update ()
+		{
+			base.Update ();
+			if (isLogin)
+			{
+				isLogin = false;
+				SceneManager.LoadSceneAsync("baccaratRoom");
+			}
+		}
+
 	}
 }
 
