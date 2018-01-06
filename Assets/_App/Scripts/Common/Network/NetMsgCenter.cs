1295fb371e25be8ea2f5b64faef7d9ddc65de7cd
Prosics <34700213+Prosics@users.noreply.github.com>
2018-01-06 15:16:13 +0800

    增加配置文件

---
+++ Assets/_App/Scripts/Common/Network/NetMsgCenter.cs
@@ -0,0 +1,18 @@
+﻿// NetMsgCenter.cs
+// Author:prosics <Prosics@163.com>
+// Date:1/6/2018
+// Copyright (c) 2018 ${Author}
+// Description:
+//
+using System;
+
+namespace GameJoy
+{
+	public class NetMsgCenter
+	{
+		public NetMsgCenter ()
+		{
+		}
+	}
+}
+
